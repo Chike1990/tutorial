6ee1d87e810e8bb44e1d01dae3766cd1180bedeb
chibuike Uzoechina <chike@chibuikes-MacBook-Pro.local>
2023-09-16 22:39:45 +1200

    counter app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+// Build a simple counter app
+// Psudocode the solution
+// import all necessary dependencies - modules and functions
+// Define your functional component (in this case CounterApp)
+// Create a state variable count to store the initial count value
+// Define a function increaseCount to increase our count value
+// Render the count value to the UI
+// export the countApp as a default export
 
-function App() {
+import React from "react";
+import { useState } from "react";
+import './App.css'
+
+const CounterApp = () => {
+  const [count, setCount] = useState(0);
+
+  const increaseCount = () =>{
+    setCount(count + 1)
+  };
+
+  const decreaseCount = () => {
+    setCount(count - 1)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="counter-container">
+      <h1>Counter</h1>
+      <div className="counter-display">{count}</div>
+      <div className="button-container">
+      <button className="increment-button" onClick={increaseCount}>increment</button>
+      <button className="increment-button" onClick={decreaseCount}>decrement</button>
+      </div>
     </div>
-  );
+  )
+
 }
 
-export default App;
+export default CounterApp;
